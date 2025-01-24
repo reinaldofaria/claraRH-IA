0cec2ca71720b1ec65d7b12a0c6a70889e0e6f3f
Reinaldo Faria <ReinaldoFaria1@MacBook-Pro-de-Reinaldo.local>
2025-01-24 10:40:31 -0300

    adicionando campos para alterar sqlite3 por pysqlite

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# Substituir o sqlite3 pelo pysqlite3 para garantir compatibilidade
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
 import tempfile
 import streamlit as st
